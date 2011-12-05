8c0bb01336211d605a4cd1e2829168391158d377
Lonnie Ezell <lonnieje@gmail.com>
2011-12-04 23:30:34 -0600

    Changed version number.

--- install/views/footer.php
+++ install/views/footer.php
@@ -4,7 +4,7 @@
 			<div class="inner">
 				<p style="float: right; margin-right: 80px;">Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
 				
-				<p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION ?></a></p>
+				<p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire 1.0</a></p>
 			</div>
 		</div>
 </body>
